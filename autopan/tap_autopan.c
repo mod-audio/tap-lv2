32406f43ad9aba917fd6c354a28522150b1f0952
pjotrompet <ploplolma@gmail.com>
2016-01-19 12:11:12 +0100

    fixed autopan again, did somthing wrong with last commit

--- autopan/tap_autopan.c
+++ autopan/tap_autopan.c
@@ -1,23 +1,18 @@
 /*                                                     -*- linux-c -*-
     Copyright (C) 2004 Tom Szilagyi
-
     Patches were received from:
         Alexander Koenig <alex@lisas.de>
-
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
     $Id: tap_autopan.c,v 1.6 2004/02/21 17:33:36 tszilagyi Exp $
 */
 
@@ -53,7 +48,6 @@
 /* cosine table for fast computations */
 float cos_table[1024];
 int flag = 0;
-float Ogain=0;
 
 
 /* The structure used to hold port connection information and state */
@@ -68,6 +62,7 @@ typedef struct {
 	float * output_R;
 	double SampleRate;
 	float Phase;
+	float Ogain;
 } AutoPan;
 
 
@@ -81,6 +76,7 @@ instantiate_AutoPan(const LV2_Descriptor * Descriptor,double SampleRate, const c
 
 	if ((ptr = malloc(sizeof(AutoPan))) != NULL) {
 		((AutoPan *)ptr)->SampleRate = SampleRate;
+		((AutoPan *)ptr)->Ogain = 0.0f;
             if(flag == 0)
 	        {
                 for (i = 0; i < 1024; i++)
@@ -157,8 +153,8 @@ run_AutoPan(LV2_Handle Instance,
 	float * output_R = ptr->output_R;
 	float freq = LIMIT(*(ptr->freq),0.0f,20.0f);
 	float depth = LIMIT(*(ptr->depth),0.0f,100.0f);
-	float gain = (db2lin(LIMIT(*(ptr->gain),-70.0f,20.0f))+Ogain)*0.5;
-	Ogain=gain;
+	float gain = (db2lin(LIMIT(*(ptr->gain),-70.0f,20.0f))+ptr->Ogain)*0.5;
+	ptr->Ogain = gain;
 	unsigned long sample_index;
 	float phase_L = 0;
 	float phase_R = 0;
